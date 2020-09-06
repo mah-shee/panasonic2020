3801001758609b861d17a14b55c345cdb94530f1
masa <masatoshi.sboard@gmail.com>
2020-09-07 07:02:26 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,14 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        a: u128,
+        b: u128,
+        c: u128,
+    }
+    let d = c - a - b;
+    if a + b < c && d.pow(2) > 4 * a * b {
+        println!("Yes");
+    } else {
+        println!("No");
     }
 }
